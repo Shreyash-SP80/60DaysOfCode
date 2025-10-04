727f1fc34b71659292a1a66d58709c85e52843e0
Shreyash <cyberninjax45@gmail.com>
2025-10-04 22:17:58 +0530

    Password generator

---
+++ Day02/scripts/password_generator.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+def generate_password(length=10):
+    # Characters: letters + digits + symbols
+    characters = string.ascii_letters + string.digits + string.punctuation
+    
+    # Randomly choose characters
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    length = int(input("Enter password length: "))
+    print("Generated Password:", generate_password(length))
