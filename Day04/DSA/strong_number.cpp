ad23f4b78ace9fc5412e27d9ea2c3d319fa85487
Shreyash <cyberninjax45@gmail.com>
2025-10-06 22:18:20 +0530

    strong number

---
+++ Day04/DSA/strong_number.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    int fact = 1;
+    for (int i = 1; i <= n; ++i)
+        fact *= i;
+    return fact;
+}
+
+int main() {
+    int num, originalNum, remainder, sum = 0;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    originalNum = num;
+
+    while (num > 0) {
+        remainder = num % 10;
+        sum += factorial(remainder);
+        num /= 10;
+    }
+
+    if (sum == originalNum)
+        cout << originalNum << " is a Strong number." << endl;
+    else
+        cout << originalNum << " is not a Strong number." << endl;
+
+    return 0;
+}
