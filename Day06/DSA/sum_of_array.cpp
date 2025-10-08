894dd19baf17a283aec8f9a2a20350c1e7299551
Shreyash <cyberninjax45@gmail.com>
2025-10-08 19:53:39 +0530

    Day06

---
+++ Day06/DSA/sum_of_array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int *arr = new int[n];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    cout << "Sum of array elements = " << sum << endl;
+    return 0;
+}
