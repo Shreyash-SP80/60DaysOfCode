5636c627b6b1e196170911de3ba300f3aa061dc4
Shreyash <cyberninjax45@gmail.com>
2025-10-08 19:55:36 +0530

    Palindrome string

---
+++ Day06/DSA/palindrome_string.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isPalindrome(string str) {
+    int left = 0;
+    int right = str.length() - 1;
+
+    while (left < right) {
+        if (str[left] != str[right])
+            return false;
+        left++;
+        right--;
+    }
+    return true;
+}
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+
+    if (isPalindrome(str))
+        cout << "Palindrome String" << endl;
+    else
+        cout << "Not a Palindrome String" << endl;
+
+    return 0;
+}
