f6a2108ae9305475389b4f4adca0c0c5c1924b2a
Shreyash <cyberninjax45@gmail.com>
2025-10-08 20:10:22 +0530

    Update sum_of_array.cpp

--- Day06/DSA/sum_of_array.cpp
+++ Day06/DSA/sum_of_array.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int n;
     cout << "Enter number of elements: ";
-    cin >> n;
+    cin >> n; 
 
     int *arr = new int[n];
     cout << "Enter " << n << " elements: ";
@@ -20,3 +20,4 @@ int main() {
     cout << "Sum of array elements = " << sum << endl;
     return 0;
 }
+
