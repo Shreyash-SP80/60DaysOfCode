9883bbcc3cceccc3348a5adb6bf1fdd368fd1c5e
Shreyash <cyberninjax45@gmail.com>
2025-10-10 15:23:30 +0530

    count characters

---
+++ Day08/DSA/count_characters.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+
+    int alphabets = 0, digits = 0, special = 0;
+
+    for (char c : str) {
+        if (isalpha(c))
+            alphabets++;
+        else if (isdigit(c))
+            digits++;
+        else
+            special++;
+    }
+
+    cout << "Alphabets: " << alphabets << endl;
+    cout << "Digits: " << digits << endl;
+    cout << "Special Characters: " << special << endl;
+
+    return 0;
+}
