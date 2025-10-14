06b61fd7eaa7880f4c46327fe338ece907a5e438
Shreyash <cyberninjax45@gmail.com>
2025-10-14 22:42:45 +0530

    Level order traversal

---
+++ Day10/DSA/Level_order_traversal.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class node {
+    public:
+        int data;     // Store the data
+        node* left;   // to store the address of left child
+        node* right;  // to store the address of right child
+
+        // Constructor for creation of node 
+        node(int data) {
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+node* buildBinaryTree(node* root) {
+    cout << "Enter data: ";
+    int data;
+    cin >> data;
+
+    root = new node(data);
+
+    if (data == -1) 
+        return NULL;
+
+    cout << "Enter data for inserting left of " << data << endl;
+    root -> left = buildBinaryTree(root->left);
+
+    cout << "Enter data for inserting right of " << data << endl;
+    root -> right = buildBinaryTree(root->right);
+
+    return root;
+}
+
+// 10 20 35 -1 -1 25 -1 -1 35 80 -1 -1 90 -1 -1
+
+void levelOrderTraversal(node *root) {
+    cout << "\nBinary Tree" << endl;
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL);
+    
+    while (!q.empty()) {
+        node* temp = q.front();
+        q.pop();
+
+        if (temp == NULL) {
+            cout << endl;
+            
+            if (!q.empty()) 
+                q.push(NULL);
+            
+        } else {
+            cout << temp->data << " ";
+            if (temp->left) 
+                q.push(temp->left);
+
+            if (temp->right)
+                q.push(temp->right);
+        }
+    }
+}
+
+int main() {
+    node* root = NULL;
+    root = buildBinaryTree(root); 
+
+    levelOrderTraversal(root); 
+    return 0;
+}
