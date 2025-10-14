efc7f3f61102cf98a190208fcc6fb85314e41ba3
Shreyash <cyberninjax45@gmail.com>
2025-10-14 22:42:06 +0530

    Day10

---
+++ Day10/DSA/Implementation_Binary_Tree.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+// Binary Tree node implementation
+class node {
+    public:
+        int data;     // Store the data
+        node* left;   // to store the address of left child
+        node* right;  // to store the address of right child
+
+        // Constructor for creation of node 
+        node(int data) {
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+node* buildBinaryTree(node* root) {
+    cout << "Enter data: ";
+    int data;
+    cin >> data;
+
+    root = new node(data);
+
+    // If data == -1 then the parent do not have any child
+    if (data == -1) 
+        return NULL;
+
+    
+    // For inserting the data left of the parent node
+    cout << "Enter data for inserting left of " << data << endl;
+    root -> left = buildBinaryTree(root->left);
+
+    // For inserting the data right of the parent node
+    cout << "Enter data for inserting right of " << data << endl;
+    root -> right = buildBinaryTree(root->right);
+
+    // After all this process return root
+    return root;
+}
+
+// 10 20 35 -1 -1 25 -1 -1 35 80 -1 -1 90 -1 -1
+
+int main() {
+
+    // Create *root object to store the all tree
+    node* root = NULL;
+    root = buildBinaryTree(root);  // Call the buildBinaryTree() to create an binary tree and returns the complete binary tree
+    cout << "Binary Tree created.." << endl;
+    return 0;
+}
