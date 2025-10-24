987c038031d07bebff8c26b9d1f83e183bda711a
Shreyash <cyberninjax45@gmail.com>
2025-10-24 22:16:47 +0530

    invert_binary_tree

---
+++ Day16/DSA/invert_binary_tree.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class TreeNode {
+public:
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int x): val(x), left(NULL), right(NULL) {}
+};
+
+TreeNode* invertTree(TreeNode* root) {
+    if (!root) return NULL;
+
+    swap(root->left, root->right);
+    invertTree(root->left);
+    invertTree(root->right);
+
+    return root;
+}
+
+void printInorder(TreeNode* root) {
+    if (!root) return;
+    printInorder(root->left);
+    cout << root->val << " ";
+    printInorder(root->right);
+}
+
+int main() {
+    TreeNode* root = new TreeNode(4);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(7);
+    root->left->left = new TreeNode(1);
+    root->left->right = new TreeNode(3);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(9);
+
+    cout << "Original Tree (Inorder): ";
+    printInorder(root);
+    cout << "\nInverted Tree (Inorder): ";
+    invertTree(root);
+    printInorder(root);
+    return 0;
+}
